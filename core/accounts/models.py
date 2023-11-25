888de7c5f9c6e88fdab73d25c3c08cb0500e1f94
Amirhesam <amirhesamshokri@gmail.com>
2023-11-26 00:37:35 +0330

    1

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,73 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    PermissionsMixin,
-    BaseUserManager,
-    AbstractUser,
-)
-from django.utils.translation import (
-    gettext_lazy as _,
-)
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
-class UserManager(BaseUserManager):
-    def create_user(self,email,password,**extra_fields):
-        if not email:
-            raise ValueError(_("the email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(email=email , **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-    
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("is_verified", True)
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)  
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
-
-
-    
-
-class User(AbstractUser,PermissionsMixin):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    phone_num = models.CharField(max_length=15)
-    email = models.EmailField(max_length=250, unique=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_verified = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    
-    USERNAME_FIELD = "email"
-    objects = UserManager()
-    REQUIRED_FIELDS = []
-    
     def __str__(self):
-        return self.email
-
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=255)
-    updated_date = models.DateTimeField(auto_now=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)    
-
-@receiver(post_save,sender=User)
-def save_profile(sender , instance , created , **kwargs):
-    if (created):
-        Profile.objects.create(user=instance)
+        return self.username
--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -123,5 +122,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "accounts.User" 
