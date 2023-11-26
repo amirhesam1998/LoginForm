9d182a2a5203a116437332d2df3c8106006b30ed
Amirhesam <amirhesamshokri@gmail.com>
2023-11-26 15:07:22 +0330

    Update Form.js

--- FrontEnd/src/Component/Form/Form.js
+++ FrontEnd/src/Component/Form/Form.js
@@ -26,7 +26,7 @@ export default function Form (){
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-                const response = await axios.post('http://127.0.0.1:8000/accounts/api/registry', formData);
+                const response = await axios.post('http://127.0.0.1:8000/accounts/api/registry/', formData);
         alert('Your information was successfully received.');
         // Optionally, you can reset the form after successful submission
         setFormData({
@@ -71,7 +71,7 @@ export default function Form (){
                   <input type="password" name="password" value={formData.password} onChange={handleChange} className="form-control" placeholder="Password" />
                 </div>
                 <div className="form-group">
-                  <input type="password" name="re_Password" value={formData.rePassword} onChange={handleChange} className="form-control" placeholder="Repeat Password" />
+                  <input type="password" name="rePassword" value={formData.rePassword} onChange={handleChange} className="form-control" placeholder="Repeat Password" />
                 </div>
                 <button type="submit" className="btn btn-primary">Register</button>
               </form>
