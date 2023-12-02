df0e2713d1cd002041a88189de5a59fac7ab5dc7
alireza rahimi <alireza.ir76@gmail.com>
2023-12-02 18:03:40 +0330

    edit1

---
+++ FrontEnd/src/Component/Page/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+import FormData from '../../data/FormData/FormData.json'
+
+export default function Login (){
+    const [data, setData] = useState(FormData)
+    
+    const handleChange =()=>{
+        
+    }
+
+}
