9a6c9fedd71512edf0e452fec7b4ba79c526a1ae
alireza rahimi <alireza.ir76@gmail.com>
2023-12-09 00:04:42 +0330

    make register correctly

--- FrontEnd/src/Component/Page/Register/Register.js
+++ FrontEnd/src/Component/Page/Register/Register.js
@@ -1,25 +1,30 @@
-import React, { useRef, useState } from "react";
+import React, {useState } from "react";
 import './Register.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from '../../../API/Axios';
+import axios from 'axios';
 import AlertColors from '../../Alert/AlertColors'
 import FormData from '../../../data/FormData/FormData.json'
 import FormGroup from '../../Form/FormGroup'
 import Form from "../../Form/Form";
 import { Link } from 'react-router-dom';
-import RenderInputs from "../../Input/Input";
-
-const REGISTER_URL = '/registry/' 
 
 export default function Register  (){
-    const formData = useRef(FormData)
+    const [formData, setFormData] = useState(FormData);
     const [errorOccurred, setErrorOccurred] = useState(false);
     const [curred, setcurred] = useState(false);
-  
+   
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
     const handleSubmit = async (e) => {
       e.preventDefault();
+      console.log("Form Data:", formData);
       try {
-        await axios.post(REGISTER_URL, formData, {
+        await axios.post('http://127.0.0.1:8000/api/registry/', FormData, {
           headers: {
             'Content-Type': 'application/json'
           }
@@ -30,13 +35,22 @@ export default function Register  (){
         
       }
     };
-
     return (
       <FormGroup>
         {curred && <AlertColors color="green" text="An error alert for showing message." />}
         {errorOccurred && <AlertColors color="red" text="A success alert for showing message." />}
         <Form layout="vertical">
-          <RenderInputs></RenderInputs>
+        {Object.keys(formData).map((fieldName) => (
+          <input
+            key={fieldName}
+            type={fieldName.includes("password") ? "password" : fieldName.includes("username") ? "username" : fieldName.includes("rePassword") ? "password" : fieldName.includes("email") ? "email" : 'text'}
+            name={fieldName}
+            value={formData[fieldName]}
+            onChange={handleChange}
+            className="form-control m-1"
+            placeholder={fieldName.charAt(0).toUpperCase() + fieldName.slice(1).replace(/_/, ' ')}
+          />
+        ))}
             <button onClick={handleSubmit} type="submit" className="btn btn-primary">Register</button>
             <Link to="/" className="btn btn-warning m-3">Login</Link>
         </Form>
