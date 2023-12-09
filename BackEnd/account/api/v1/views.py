61697a64a32bbe2bb474bf97db91c6d2a61bb9ce
Amirhesam <amirhesamshokri@gmail.com>
2023-12-09 13:09:52 +0330

    registry update1

--- BackEnd/account/api/v1/views.py
+++ BackEnd/account/api/v1/views.py
@@ -9,6 +9,7 @@ class UserRegistryApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
 
     def post(self, request, *args, **kwargs):
+        print(data=request.data)
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -17,3 +18,6 @@ class UserRegistryApiView(generics.GenericAPIView):
             user_obj = get_object_or_404(CustomUser, email=email)
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserLoginApiView(generics.GenericAPIView):
+    pass
