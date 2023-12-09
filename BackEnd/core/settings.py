3b632bf77f9846ea577ece07e98145abcd7b461e
Amirhesam <amirhesamshokri@gmail.com>
2023-12-09 13:12:31 +0330

    Update settings.py

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -133,4 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+)
