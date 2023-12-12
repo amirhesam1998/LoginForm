e0557a193470812e793bd377e0768be0eed69363
alireza rahimi <alireza.ir76@gmail.com>
2023-12-12 13:04:44 +0330

    Update Authentication.js

--- FrontEnd/src/Context/Authentication.js
+++ FrontEnd/src/Context/Authentication.js
@@ -1,5 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react';
+import Axios from '../API/Axios';
 
+const LOGOUT_URL = '/token/logout/'
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
@@ -15,14 +17,23 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     localStorage.setItem('token', auth.token);
     localStorage.setItem('first_name',auth.first_name)
-  }, [auth.token]);
+  }, [auth.token,auth.first_name]);
 
   const login = (username, token, first_name) => {
     setAuth({ username, token, first_name });
   };
 
-  const logout = () => {
-    setAuth({ username: '', token: '', first_name: '' });
+  const logout = async (e) => {
+    try {
+      await Axios.post(LOGOUT_URL, auth.token, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      localStorage.removeItem('token');
+      localStorage.removeItem('first_name');
+      setAuth({ username: '', token: '', first_name: '' });
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
   };
 
   return (
